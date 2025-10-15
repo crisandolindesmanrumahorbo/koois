b670a09f794a6cadecb0de4fc5ceefd195eb6ebd
crisandolindesmanrumahorbo <crisandolin007@gmail.com>
2025-10-15 16:52:22 +0700

    sign in with google service

--- app/[lang]/login/service.ts
+++ app/[lang]/login/service.ts
@@ -1,5 +1,10 @@
 "use server";
 
+interface ISignGoogleResponse {
+  token: string;
+  is_registered: boolean;
+}
+
 export const login = async (username: string, password: string) => {
   const body = JSON.stringify({
     username,
@@ -11,6 +16,25 @@ export const login = async (username: string, password: string) => {
   });
   if (response.status >= 200 && response.status < 300) {
     const data = await response.json();
+    return { data, error: null };
+  }
+  if (response.status >= 400 && response.status < 500) {
+    return { data: null, error: "credential is wrong" };
+  }
+  return { data: null, error: "server have problem," };
+};
+
+export const sign_google = async (token: string) => {
+  const body = JSON.stringify({
+    token,
+  });
+  const response = await fetch(`${process.env.BASE_URL}/signin-google`, {
+    method: "POST",
+    body,
+  });
+  if (response.status >= 200 && response.status < 300) {
+    const data = (await response.json()) as ISignGoogleResponse;
+    console.log("signin-google ", data);
     return { data, error: null };
   }
   if (response.status >= 400 && response.status < 500) {
